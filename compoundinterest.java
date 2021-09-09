11c1f70d46b3418fe72ea5901421ed7a63457f9d
alceil <47685349+alceil@users.noreply.github.com>
2021-09-09 16:08:50 +0530

    Create compoundinterest.java

---
+++ compoundinterest.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	  double amount=0,principle,rate,time,ci,t=1;		
+ 
+	 
+	
+	Scanner sc=new Scanner(System.in);
+	
+	System.out.println("enter principle ");
+	
+	principle=sc.nextDouble();
+	
+	System.out.println("enter rate");
+ 
+	rate=sc.nextDouble();
+	
+	System.out.println("enter time");
+	
+	time=sc.nextDouble();
+		
+	rate=(1+rate/100);
+               int i=0;
+         while(i<time)
+        {
+	   t*=rate;
+           i++;
+	 }
+ 
+	amount=principle*t;
+ 
+	System.out.println("amount="+amount);
+ 
+	ci=amount-principle;
+ 
+	System.out.println("compound interest="+ci);
+    }
+}
