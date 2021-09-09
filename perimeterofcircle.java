e0044c7523a91419127293331807a93145f26620
alceil <47685349+alceil@users.noreply.github.com>
2021-09-09 14:27:13 +0530

    Create perimeterofcircle.java

---
+++ perimeterofcircle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+ Scanner s= new Scanner(System.in);
+        
+         System.out.println("Enter the radius:");
+         
+	 double r= s.nextDouble();
+         
+	 double  c=(22*2*r)/7 ;
+      
+	 System.out.println("Perimeter of Circle is: " +c);    
+    }
+}
