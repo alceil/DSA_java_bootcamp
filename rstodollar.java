7a9dfbd637a751e392d376526f840ab4dc5d3e99
alceil <47685349+alceil@users.noreply.github.com>
2021-09-09 11:52:50 +0530

    Create rstodollar.java

---
+++ rstodollar.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+   int rs;
+    Scanner input  = new Scanner(System.in);
+    rs = input.nextInt();
+    System.out.println(rs /72+"$");
+    
+  
+
+    }
+}
