a9986b4f3913a8cf34c2cb53c811f1ac9d53e523
alceil <47685349+alceil@users.noreply.github.com>
2021-09-09 11:41:03 +0530

    Create printname.java

---
+++ printname.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+   String name;
+    Scanner input  = new Scanner(System.in);
+    name = input.nextLine();
+    System.out.println("Hello "+name);
+    }
+}
