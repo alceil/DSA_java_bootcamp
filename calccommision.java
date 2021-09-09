2018154f250ce53e6ca3c5e00f9ea9784c66a7c8
alceil <47685349+alceil@users.noreply.github.com>
2021-09-09 15:59:36 +0530

    Create calccommision.java

---
+++ calccommision.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	  Scanner sc=new Scanner(System.in);
+ 
+	  System.out.print("Enter amount:");
+ 
+	  double amount=sc.nextDouble();
+ 
+	  System.out.print("Enter commissionPercentage:");
+ 
+          double commissionPercentage=sc.nextDouble();
+ 
+	  double commission=(commissionPercentage/100)*amount;  	   
+ 
+      	  System.out.println("Commission amount="+commission);
+    }
+}
