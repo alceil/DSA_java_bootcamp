b7b46a22c7055d3e660d6cc8f44e4d00cf3b270d
alceil <47685349+alceil@users.noreply.github.com>
2021-09-09 14:37:53 +0530

    Create perofrhombus.java

---
+++ perofrhombus.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+ Scanner s= new Scanner(System.in);
+        
+         System.out.println("Enter the side of the Rhombus:");
+         
+         double a= s.nextDouble();
+	 
+         double  perimeter=4*a;
+      
+	 System.out.println("perimeter of Rhombus is: " + perimeter);   
+    }
+}
