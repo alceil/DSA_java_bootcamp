cdc6873aba950701ee3650b10db544e613d88641
alceil <47685349+alceil@users.noreply.github.com>
2021-09-09 14:15:49 +0530

    Create areaofiso.java

---
+++ areaofiso.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+       Scanner s= new Scanner(System.in);
+        
+         System.out.println("Enter the length of same sided");
+         
+         double a= s.nextDouble();
+         
+         System.out.println("Enter the side2 of the Triangle");
+         
+         double b= s.nextDouble();
+ 
+         double  area=(b/4)*Math.sqrt((4*a*a)-(b*b));
+      
+         System.out.println("Area of Isosceles Triangle is: " + area);
+    }
+}
