a85b21db465fc37b59b8b8015de27d389637284e
alceil <47685349+alceil@users.noreply.github.com>
2021-09-09 14:36:05 +0530

    Create perimeterofrec.java

---
+++ perimeterofrec.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+Scanner s= new Scanner(System.in);
+        
+         System.out.println("Enter the length of the Rectangle:");
+         
+	 double l= s.nextDouble();
+	 
+	 System.out.println("Enter the width of the Rectangle:");
+         
+	 double b= s.nextDouble();
+          
+         double  perimeter=2*(l+b);
+      
+	 System.out.println("perimeter of Rectangle is: " + perimeter);      
+    }
+}
