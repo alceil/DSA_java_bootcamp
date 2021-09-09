c4c818a0af6adba3506edf8ca7f3310ebe7af91f
alceil <47685349+alceil@users.noreply.github.com>
2021-09-09 11:45:19 +0530

    Create simpleinterest.java

---
+++ simpleinterest.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+   int p,r,t;
+    Scanner input  = new Scanner(System.in);
+    p = input.nextInt();
+    r = input.nextInt();
+    t = input.nextInt();
+    System.out.println("SI "+p*r*t*.01);
+    }
+}
