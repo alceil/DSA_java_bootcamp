5d6df13e288135b542674ecbc0dbf1d12bab5bd0
alceil <47685349+alceil@users.noreply.github.com>
2021-09-09 14:41:36 +0530

    Create volofcyl.java

---
+++ volofcyl.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+ Scanner s= new Scanner(System.in);
+        
+         System.out.println("Enter the radius:");
+         double r=s.nextDouble();
+         System.out.println("Enter the height:");
+         double h=s.nextDouble();
+ 
+            double  volume=((22*r*r*h)/7);
+ 
+            System.out.println("volume of Cylinder is: " +volume);
+       
+    }
+}
