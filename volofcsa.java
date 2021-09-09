dab6779eae187a9c7de16a5f5b042107e5c13091
alceil <47685349+alceil@users.noreply.github.com>
2021-09-09 14:46:02 +0530

    Create volofcsa.java

---
+++ volofcsa.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+ Scanner s= new Scanner(System.in);
+        
+         	System.out.println("Enter the radius:");
+         	
+      		double r=s.nextDouble();
+         	
+		System.out.println("Enter the height:");
+         	
+		double h=s.nextDouble();
+ 
+            	double  tsa=((2*22*r)/7)*(r+h);
+ 
+            	System.out.println("TotalSurfaceArea Of Cylinder is:  " + tsa);
+       
+    }
+}
