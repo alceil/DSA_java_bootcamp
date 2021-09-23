ff4954569678f5f8b804f4efbde8572045c797ed
alceil <47685349+alceil@users.noreply.github.com>
2021-09-23 08:35:38 +0530

    Create firstandlastelement.java

---
+++ firstandlastelement.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int [] ans ={-1,-1};
+        ans[0] = search(nums,target,true);
+        if(ans[0]!=-1)
+        {
+            ans[1]=search(nums,target,false);
+        }
+        return ans;
+    }
+    
+    int search(int [] nums,int target , boolean findStartIndex)
+    {
+        int ans = -1;
+        int mid =0;
+        int start = 0;
+        int end = nums.length - 1;
+        while(start<=end)
+        {
+            mid = start + (end-start)/2;
+            
+            if(target<nums[mid])
+            {
+                end = mid - 1;
+                
+            }else if(target>nums[mid]){
+                start = mid + 1;
+            }else{
+                ans = mid;
+                if(findStartIndex){
+                  end = mid-1;  
+                }else{
+                    start = mid +1;
+                    
+                }
+            }
+            
+        }
+        return ans;
+    }
+}
