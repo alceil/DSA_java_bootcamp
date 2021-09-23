512f5c6acf2fdaaafaa48a54fd304ca1bc29aa5f
alceil <47685349+alceil@users.noreply.github.com>
2021-09-23 09:52:35 +0530

    Create singlenonduplicate.java

---
+++ singlenonduplicate.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+          int start = 0;
+        int end = nums.length-1;
+        
+        if(nums.length == 0) return 0;
+        else if(nums.length ==1) return nums[0];
+        else if(nums[0] != nums[1]) return nums[0];
+        else if(nums[end] != nums[end-1]) return nums[end];
+        
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
+            
+            else if(mid % 2 == 0) {
+                if(nums[mid] == nums[mid+1]) start = mid + 1;
+                else end = mid - 1;
+            }
+            else if(mid % 2 != 0) {
+                if(nums[mid] == nums[mid-1]) start = mid +1;
+                else end = mid - 1;
+            }   
+        }return -1;
+    }
+}
