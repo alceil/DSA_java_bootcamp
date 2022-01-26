995a5418b8a12b7566699d59b9df20ffdad94520
alceil <47685349+alceil@users.noreply.github.com>
2022-01-26 08:55:22 +0530

    Create basiccalc.java

---
+++ basiccalc.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int calculate(String s) {
+        int len = s.length();
+        if(s==null || len==0) return 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        int currentNumber = 0;
+        char operation = '+';
+        for(int i = 0;i<len;i++)
+        {
+            char currentChar = s.charAt(i);
+            if(Character.isDigit(currentChar))
+            {
+                currentNumber = (currentNumber*10) + (currentChar - '0');
+            }
+            if(!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i==len-1)
+            {
+                if(operation=='-')
+                {
+                    stack.push(-currentNumber);
+                }
+                else if(operation=='+')
+                {
+                            stack.push(currentNumber);
+                }
+                else if(operation=='*')
+                {
+                       stack.push(stack.pop()*currentNumber);
+                }
+                  else if(operation=='/')
+                {
+                       stack.push(stack.pop()/currentNumber);
+                }
+                operation = currentChar;
+                currentNumber = 0;
+            }
+        }
+        int result = 0;
+        while(!stack.isEmpty())
+        {
+            result += stack.pop();
+        }
+        return result;      
+    }
+}
