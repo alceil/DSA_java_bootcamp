8ff6bbfffde061160bddb61013656adecc599c40
alceil <47685349+alceil@users.noreply.github.com>
2022-01-27 09:56:42 +0530

    Create reversewordsinastring.java

---
+++ reversewordsinastring.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String reverseWords(String s) {
+        
+        
+        s = s.trim();
+        List<String> wordList = Arrays.asList(s.split("\\s+"));
+        Collections.reverse(wordList);
+        return String.join(" ",wordList);
+        
+        
+
+    }
+}
